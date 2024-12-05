276845415ce53ed6658add94d3d7d25428b58f85
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-05 18:34:34 +0700

    feat(apollo): Add resolvers and local db

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone"
 import { typeDefs } from "./schema.js";
+import db from "./_db.js"
 
-// Server setup
+// ===== Resolvers =====
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        }
+    }
+}
+
+// ===== Server setup =====
 const PORT = 4000
 const server = new ApolloServer({
     // typeDefs
     typeDefs,
     // Resolvers
+    resolvers,
 })
 
 const { url } = await startStandaloneServer(server, {
