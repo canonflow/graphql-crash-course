515caf404566217d0b4119df1bbc9ddb766595a9
Nathan Garzya Santoso <85006633+canonflow@users.noreply.github.com>
2024-12-05 20:36:30 +0700

    feat(resolvers): Add relationship

--- index.js
+++ index.js
@@ -9,20 +9,39 @@ const resolvers = {
         games() {
             return db.games
         },
+        game(parent, args) {
+            return db.games.find((game) => game.id === args.id)
+        },
         reviews() {
             return db.reviews
-        },
-        authors() {
-            return db.authors
         },
         review(parent, args) {
             return db.reviews.find((review) => review.id === args.id)
         },
-        game(parent, args) {
-            return db.games.find((game) => game.id === args.id)
+        authors() {
+            return db.authors
         },
         author(parent, args) {
             return db.authors.find((author) => author.id === args.id)
+        }
+    },
+    // For nested query
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.author_id === parent.id)
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((author) => author.id === parent.author_id)
+        },
+        game(parent) {
+            return db.games.find((game) => game.id === parent.game_id)
         }
     }
 }
